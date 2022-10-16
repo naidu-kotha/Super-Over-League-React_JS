71f7f3399f34fe54e0d9d7d8f6063fb9475b5ada
naidu-kotha <srinivasnaidu1605@gmail.com>
2022-10-16 13:20:35 +0000

    CSS flexbooc Solution

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const element = (
-//   Write your code here.
-    <div className = "container-fluid bg pt-5 pb-5">
-        <div className = "row">
-            <div className = "col">
-                <div className = "d-flex flex-column justify-content-center text-center m-auto">
-                    <h1 className = "heading">Super Over League</h1>
-                    <div className="d-flex flex-row justify-content-center">
-                        <img src = "https://assets.ccbp.in/frontend/react-js/rcb-img.png" className="w-25" />
-                        <img src = "https://assets.ccbp.in/frontend/react-js/csk-img.png" className= "w-25" />
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div className="super-over-league-container">
+    <h1 className="heading">Super Over League</h1>
+    <div className="teams-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="logo-image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="logo-image"
+      />
     </div>
-
+  </div>
 )
 
 ReactDOM.render(element, document.getElementById('root'))
-
